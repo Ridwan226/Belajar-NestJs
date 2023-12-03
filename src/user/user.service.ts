85e0bc4a697af172dc4a2ed476cd9d7b425da828
ridwanromadhon <ridwanromadhon48@gmail.com>
2023-12-03 21:36:43 +0700

    Add User

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma.service';
+import { hash } from 'bcryptjs';
 
 @Injectable()
 export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async register(createUserDto: CreateUserDto) {
+    const newUser = await this.prisma.user.create({
+      data: {
+        ...createUserDto,
+        password: await hash(createUserDto.password, 10),
+      },
+    });
+    const { password, ...user } = newUser;
+    return user;
+  }
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
